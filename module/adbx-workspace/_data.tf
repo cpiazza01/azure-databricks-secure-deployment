1e060b6c8ff78d33b70a4917041641a40ad6f338
Cody Piazza <codypiazza@gmail.com>
2025-07-29 22:01:16 -0500

    add destroy

--- module/adbx-workspace/_data.tf
+++ module/adbx-workspace/_data.tf
@@ -1,5 +1,5 @@
 data "azurerm_client_config" "current" {}
 
 data "azuread_user" "current_user" {
-    object_id = data.azurerm_client_config.current.object_id
+  object_id = data.azurerm_client_config.current.object_id
 }
--- module/adbx-workspace/_provider.tf
+++ module/adbx-workspace/_provider.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "azure-databricks-rg"
     storage_account_name = "cpiazza01shared"
     container_name       = "cpiazza01-tf-state"
+    key                  = "adbx-workspace.tfstate"
   }
   required_providers {
     azurerm = {
