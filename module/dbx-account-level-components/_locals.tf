a258e3c223a58019fae044bf08f9f876503d565d
Cody Piazza <codypiazza@gmail.com>
2025-10-06 16:39:58 -0600

    debug

--- module/dbx-account-level-components/_locals.tf
+++ module/dbx-account-level-components/_locals.tf
@@ -2,8 +2,4 @@ locals {
   current_module_path = path.module
   parent_folder_path  = dirname(local.current_module_path)
   module_name         = basename(local.parent_folder_path)
-
-  prefix           = "cpiazza-azure-databricks"
-  dbfsname_transit = "cpiazzadbfstransit"
-  dbfsname_app     = "cpiazzadbfsapp"
 }
