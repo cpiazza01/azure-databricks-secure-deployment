b57b6ae83299c13eeb41464bcbd89d0f31520c1f
Cody Piazza <codypiazza@gmail.com>
2025-10-07 15:47:31 -0600

    test groups

--- module/dbx-account-level-components/_data.tf
+++ module/dbx-account-level-components/_data.tf
@@ -18,8 +18,8 @@ data "azuread_groups" "databricks_group_objects" {
 }
 
 data "azuread_group" "databricks_groups" {
-  for_each = {for key, value in data.azuread_groups.databricks_group_objects.object_ids: value.object_id => value.display_name }
-  object_id = each.key
+  for_each = {for key, value in data.azuread_groups.databricks_group_objects.object_ids: value => value }
+  object_id = each.value
 }
 
 output "databricks_groups" {
