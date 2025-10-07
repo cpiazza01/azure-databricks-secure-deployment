8c477db174015cc4a3a1f42aee809c1d663dbe12
Cody Piazza <codypiazza@gmail.com>
2025-10-06 18:20:45 -0600

    debug

--- module/dbx-account-level-components/dbx_metastore_assignment.tf
+++ module/dbx-account-level-components/dbx_metastore_assignment.tf
@@ -1,4 +1,4 @@
 resource "databricks_metastore_assignment" "this" {
-  metastore_id = databricks_metastore.eastus.id
+  metastore_id = data.databricks_metastore.eastus.id
   workspace_id = local.workspace_id
 }
