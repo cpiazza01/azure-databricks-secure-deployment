23c0d4a23c8aead6d29e04a2336bf722bfafb923
Cody Piazza <codypiazza@gmail.com>
2025-10-08 21:43:38 -0600

    fix

--- module/dbx-account-level-components/_provider.tf
+++ module/dbx-account-level-components/_provider.tf
@@ -1,10 +1,5 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "azure-databricks-rg"
-    storage_account_name = "cpiazza01dbxtfstate"
-    container_name       = "tfstate"
-    key                  = "dbx-account-level-components.tfstate"
-  }
+  backend "azurerm" {}
   required_providers {
     databricks = {
       source  = "databricks/databricks"
