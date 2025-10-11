680cd16c062fb314b3b1c37e73251481fa7b657b
Cody Piazza <codypiazza@gmail.com>
2025-10-10 18:10:41 -0600

    debug

--- module/dbx-account-level-components/dbx_service_principals.tf
+++ module/dbx-account-level-components/dbx_service_principals.tf
@@ -8,8 +8,14 @@ resource "databricks_service_principal" "ro" {
   display_name = "SP_RO_${each.value.display_name}"
 }
 
-resource "databricks_group_member" "catalog_users_sps" {
-  for_each  = concat(databricks_service_principal.rw, databricks_service_principal.ro)
+resource "databricks_group_member" "catalog_users_sps_rw" {
+  for_each  = databricks_service_principal.rw
+  group_id  = databricks_group.catalog_users.id
+  member_id = each.value.id
+}
+
+resource "databricks_group_member" "catalog_users_sps_ro" {
+  for_each  = databricks_service_principal.ro
   group_id  = databricks_group.catalog_users.id
   member_id = each.value.id
 }
