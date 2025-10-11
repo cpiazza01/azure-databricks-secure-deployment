024a209e6f0c4aec10e6b6857a87645df9257e00
Cody Piazza <codypiazza@gmail.com>
2025-10-10 18:55:45 -0600

    add metastore perms

--- module/cdp-account-level-components/dbx_service_principals.tf
+++ module/cdp-account-level-components/dbx_service_principals.tf
@@ -1,10 +1,19 @@
+resource "databricks_service_principal" "workspace_admin_sp" {
+  display_name = "SP_CDP_WORKSPACE_ADMIN_${upper(var.env)}"
+}
+
+resource "databricks_group_member" "workspace_admin_sp_in_admin_group" {
+  group_id  = local.workspace_admin_group.id
+  member_id = databricks_service_principal.workspace_admin_sp.id
+}
+
 resource "databricks_service_principal" "rw" {
-  for_each     = databricks_group.entra_groups
+  for_each     = toset([for group in databricks_group.entra_groups: group if !strcontains(group.display_name, "WORKSPACE_ADMIN")])
   display_name = "SP_RW_${each.value.display_name}"
 }
 
 resource "databricks_service_principal" "ro" {
-  for_each     = databricks_group.entra_groups
+  for_each     = toset([for group in databricks_group.entra_groups: group if !strcontains(group.display_name, "WORKSPACE_ADMIN")])
   display_name = "SP_RO_${each.value.display_name}"
 }
 
