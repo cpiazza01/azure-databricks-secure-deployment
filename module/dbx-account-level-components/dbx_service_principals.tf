ce9c315fd2dd065b00c8bfad4cfbf796609264c7
Cody Piazza <codypiazza@gmail.com>
2025-10-10 18:07:31 -0600

    add corresponding SPs

---
+++ module/dbx-account-level-components/dbx_service_principals.tf
@@ -0,0 +1,15 @@
+resource "databricks_service_principal" "rw" {
+  for_each     = databricks_group.entra_groups
+  display_name = "SP_RW_${each.value.display_name}"
+}
+
+resource "databricks_service_principal" "ro" {
+  for_each     = databricks_group.entra_groups
+  display_name = "SP_RO_${each.value.display_name}"
+}
+
+resource "databricks_group_member" "catalog_user_members" {
+  for_each  = concat(databricks_service_principal.rw, databricks_service_principal.ro)
+  group_id  = databricks_group.catalog_users.id
+  member_id = each.value.id
+}
