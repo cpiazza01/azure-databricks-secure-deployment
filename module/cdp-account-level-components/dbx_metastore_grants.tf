a760ee01e066d008da9a6a629ecf4d9408982249
Cody Piazza <codypiazza@gmail.com>
2025-10-10 19:05:28 -0600

    debug

--- module/cdp-account-level-components/dbx_metastore_grants.tf
+++ module/cdp-account-level-components/dbx_metastore_grants.tf
@@ -1,6 +1,5 @@
 resource "databricks_grant" "workspace_admin_metastore_grants" {
-  metastore = data.databricks_metastore.eastus.id
-
+  metastore  = data.databricks_metastore.eastus.id
   principal  = local.workspace_admin_group.display_name
   privileges = [
     "CREATE_EXTERNAL_LOCATION",
