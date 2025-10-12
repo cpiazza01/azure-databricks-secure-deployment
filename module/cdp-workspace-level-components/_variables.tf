8f9b9819d5dfd9c7be0c44cce70ca473f5f88e35
Cody Piazza <codypiazza@gmail.com>
2025-10-12 17:25:02 -0600

    DEBUG

--- module/cdp-workspace-level-components/_variables.tf
+++ module/cdp-workspace-level-components/_variables.tf
@@ -20,7 +20,7 @@ variable "storage_account_acceses_to_grant" {
   type = list(string)
   default = [
     "Storage Account Contributor",
-    # "Storage Blob Data Contributor",
+    "Storage Blob Data Contributor",
     # "Storage Queue Data Contributor",
     "EventGrid EventSubscription Contributor"
   ]
--- module/cdp-workspace-level-components/dbx_external_locations.tf
+++ module/cdp-workspace-level-components/dbx_external_locations.tf
@@ -2,10 +2,12 @@ resource "databricks_external_location" "cdp_catalog_root_ext_loc" {
   name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_root.name, azurerm_storage_account.cdp_catalog_root_storage_account.name)
   credential_name = local.cdp_storage_credential.id
+  depends_on      = [azurerm_role_assignment.storage_account_access_catalog_root]
 }
 
 resource "databricks_external_location" "cdp_catalog_stage_ext_loc" {
   name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_stage.name, azurerm_storage_account.cdp_stage_storage_account.name)
   credential_name = local.cdp_storage_credential.id
+  depends_on      = [azurerm_role_assignment.storage_account_access_stage]
 }
