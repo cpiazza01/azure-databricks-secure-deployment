4239bc05179a9700692dbf7dba854ac45b1604a8
Cody Piazza <codypiazza@gmail.com>
2025-10-11 18:44:17 -0600

    debug

--- module/cdp-account-level-components/azurerm_storage_containers.tf
+++ module/cdp-account-level-components/azurerm_storage_containers.tf
@@ -1,4 +1,10 @@
 resource "azurerm_storage_container" "cdp_catalog" {
-  name               = "cdp_catalog"
+  name               = "cdp-catalog"
   storage_account_id = data.azurerm_storage_account.cdpdatabrick.id
+}
+
+resource "azurerm_role_assignment" "container_access" {
+  scope                = azurerm_storage_container.cdp_catalog.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_databricks_access_connector.cdp_access_connector.identity.principal_id
 }
--- module/cdp-account-level-components/dbx_storage_credential.tf
+++ module/cdp-account-level-components/dbx_storage_credential.tf
@@ -8,12 +8,6 @@ resource "azurerm_databricks_access_connector" "cdp_access_connector" {
   }
 }
 
-resource "azurerm_role_assignment" "container_access" {
-  scope                = azurerm_storage_container.cdp_catalog.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_databricks_access_connector.cdp_access_connector.identity.principal_id
-}
-
 resource "databricks_storage_credential" "cdp_storage_credential" {
   name = "cdp_storage_credential"
   azure_managed_identity {
