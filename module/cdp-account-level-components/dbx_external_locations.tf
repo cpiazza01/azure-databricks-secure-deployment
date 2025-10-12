ce0a85d7e67389340c3e9f640c838725cfab495c
Cody Piazza <codypiazza@gmail.com>
2025-10-12 12:45:50 -0600

    debug

--- module/cdp-account-level-components/dbx_external_locations.tf
+++ module/cdp-account-level-components/dbx_external_locations.tf
@@ -1,11 +1,7 @@
-# resource "databricks_external_location" "cdp_catalog" {
-#   name = "cdp_catalog_${lower(var.env)}"
-#   url = format("abfss://%s@%s.dfs.core.windows.net",
-#     azurerm_storage_container.ext_storage.name,
-#   azurerm_storage_account.ext_storage.name)
-#   credential_name = databricks_storage_credential.external.id
-#   comment         = "Managed by TF"
-#   depends_on = [
-#     databricks_metastore_assignment.this
-#   ]
-# }
+resource "databricks_external_location" "cdp_catalog" {
+  name = "cdp_catalog_${lower(var.env)}"
+  url = format("abfss://%s@%s.dfs.core.windows.net",
+    data.azurerm_storage_container.cdp_catalog.name,
+  data.azurerm_storage_account.cdpdatabrick.name)
+  credential_name = databricks_storage_credential.cdp_storage_credential.id
+}
