2a2b35bf0dd9caa28bf3a49a2952a4d658ec4758
Cody Piazza <codypiazza@gmail.com>
2025-10-12 15:16:22 -0600

    updates to account-levl comps

--- module/cdp-account-level-components/dbx_external_locations.tf
+++
@@ -1,7 +0,0 @@
-resource "databricks_external_location" "cdp_catalog" {
-  provider        = databricks.dbx_workspace
-  depends_on      = [databricks_mws_permission_assignment.account_admin]
-  name            = "cdp_catalog_${lower(var.env)}"
-  url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog.name, data.azurerm_storage_account.cdpdatabrick.name)
-  credential_name = databricks_storage_credential.cdp_storage_credential.id
-}
---
+++ module/cdp-account-level-components/dbx_metastore_grants.tf
@@ -0,0 +1,17 @@
+resource "databricks_grant" "workspace_admin_metastore_grants" {
+  provider   = databricks.dbx_workspace
+  depends_on = [databricks_mws_permission_assignment.account_admin]
+  metastore  = data.databricks_metastore.eastus.id
+  principal  = local.workspace_admin_group.display_name
+  privileges = [
+    "CREATE_RECIPIENT",
+    "CREATE_PROVIDER",
+    "CREATE_SHARE",
+    "CREATE_CONNECTION",
+    "CREATE_SERVICE_CREDENTIAL",
+    "CREATE_STORAGE_CREDENTIAL",
+    "CREATE_EXTERNAL_LOCATION",
+    "CREATE_FOREIGN_CATALOG",
+    "CREATE_CATALOG"
+  ]
+}
