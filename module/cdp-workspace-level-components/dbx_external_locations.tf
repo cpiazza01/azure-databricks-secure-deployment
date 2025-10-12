cc68b94fd94d10dbffd047174eb5b91f7441c6e5
Cody Piazza <codypiazza@gmail.com>
2025-10-12 16:42:59 -0600

    debug

--- module/cdp-workspace-level-components/dbx_external_locations.tf
+++ module/cdp-workspace-level-components/dbx_external_locations.tf
@@ -1,11 +1,11 @@
-resource "databricks_external_location" "cdp_catalog_root_ext_loc" {
-  name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
-  url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_root.name, azurerm_storage_account.cdp_catalog_root_storage_account.name)
-  credential_name = databricks_storage_credential.cdp_storage_credential.id
-}
+# resource "databricks_external_location" "cdp_catalog_root_ext_loc" {
+#   name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
+#   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_root.name, azurerm_storage_account.cdp_catalog_root_storage_account.name)
+#   credential_name = databricks_storage_credential.cdp_storage_credential.id
+# }
 
-resource "databricks_external_location" "cdp_catalog_stage_ext_loc" {
-  name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
-  url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_stage.name, azurerm_storage_account.cdp_stage_storage_account.name)
-  credential_name = databricks_storage_credential.cdp_storage_credential.id
-}
+# resource "databricks_external_location" "cdp_catalog_stage_ext_loc" {
+#   name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
+#   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_stage.name, azurerm_storage_account.cdp_stage_storage_account.name)
+#   credential_name = databricks_storage_credential.cdp_storage_credential.id
+# }
