b68fba76ffd0087d82361353fa986bfd2e004a40
Cody Piazza <codypiazza@gmail.com>
2025-10-12 17:40:32 -0600

    DEBUG

--- module/cdp-workspace-level-components/dbx_external_locations.tf
+++ module/cdp-workspace-level-components/dbx_external_locations.tf
@@ -1,23 +1,13 @@
-resource "time_sleep" "wait_until_access" {
-  create_duration = "10s"
-  depends_on = [
-    azurerm_role_assignment.storage_account_access_catalog_root,
-    azurerm_role_assignment.storage_account_access_stage
-  ]
-}
-
 resource "databricks_external_location" "cdp_catalog_root_ext_loc" {
-  name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
+  name            = "cdp_catalog_root_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_root.name, azurerm_storage_account.cdp_catalog_root_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  skip_validation = true
-  depends_on      = [time_sleep.wait_until_access]
+  depends_on      = [azurerm_role_assignment.storage_account_access_catalog_root]
 }
 
 resource "databricks_external_location" "cdp_catalog_stage_ext_loc" {
   name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_stage.name, azurerm_storage_account.cdp_stage_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  skip_validation = true
-  depends_on      = [time_sleep.wait_until_access]
+  depends_on      = [azurerm_role_assignment.storage_account_access_stage]
 }
