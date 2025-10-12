c39fb61160ae45cd2f559917b9f16859580e39b3
Cody Piazza <codypiazza@gmail.com>
2025-10-12 17:37:25 -0600

    DEBUG

--- module/cdp-workspace-level-components/dbx_external_locations.tf
+++ module/cdp-workspace-level-components/dbx_external_locations.tf
@@ -10,6 +10,7 @@ resource "databricks_external_location" "cdp_catalog_root_ext_loc" {
   name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_root.name, azurerm_storage_account.cdp_catalog_root_storage_account.name)
   credential_name = local.cdp_storage_credential.id
+  skip_validation = true
   depends_on      = [time_sleep.wait_until_access]
 }
 
@@ -17,5 +18,6 @@ resource "databricks_external_location" "cdp_catalog_stage_ext_loc" {
   name            = "cdp_catalog_stage_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_stage.name, azurerm_storage_account.cdp_stage_storage_account.name)
   credential_name = local.cdp_storage_credential.id
+  skip_validation = true
   depends_on      = [time_sleep.wait_until_access]
 }
