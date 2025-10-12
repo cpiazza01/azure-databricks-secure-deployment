3a9a7e861f90ce7bfb45fbaeef9de9b45c291879
Cody Piazza <codypiazza@gmail.com>
2025-10-12 12:50:34 -0600

    debug

--- module/cdp-account-level-components/dbx_external_locations.tf
+++ module/cdp-account-level-components/dbx_external_locations.tf
@@ -1,7 +1,5 @@
 resource "databricks_external_location" "cdp_catalog" {
   name = "cdp_catalog_${lower(var.env)}"
-  url = format("abfss://%s@%s.dfs.core.windows.net",
-    data.azurerm_storage_container.cdp_catalog.name,
-  data.azurerm_storage_account.cdpdatabrick.name)
+  url = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog.name, data.azurerm_storage_account.cdpdatabrick.name)
   credential_name = databricks_storage_credential.cdp_storage_credential.id
 }
