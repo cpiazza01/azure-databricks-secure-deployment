908f3d8a2544974387ab82d2faf5c5645d808034
Cody Piazza <codypiazza@gmail.com>
2025-10-14 16:13:41 -0600

    remove groups from catalog users group, as they can be added via other groups instead later

--- module/cdp-account-level-components/dbx_groups.tf
+++ module/cdp-account-level-components/dbx_groups.tf
@@ -1,7 +1,6 @@
 resource "databricks_group" "catalog_users" {
   display_name = "CDP_CATALOG_USERS_${upper(var.env)}"
 }
-
 resource "databricks_entitlements" "catalog_users_entitlements" {
   provider              = databricks.dbx_workspace
   depends_on            = [databricks_mws_permission_assignment.account_admin]
@@ -15,11 +14,11 @@ resource "databricks_group" "entra_groups" {
   display_name = each.value.display_name
   external_id  = each.value.object_id
 }
-resource "databricks_group_member" "catalog_users_entra_groups" {
-  for_each  = databricks_group.entra_groups
-  group_id  = databricks_group.catalog_users.id
-  member_id = each.value.id
-}
+# resource "databricks_group_member" "catalog_users_entra_groups" {
+#   for_each  = databricks_group.entra_groups
+#   group_id  = databricks_group.catalog_users.id
+#   member_id = each.value.id
+# }
 
 # Bronze layer Access Groups
 resource "databricks_group" "bronze_rw" {
