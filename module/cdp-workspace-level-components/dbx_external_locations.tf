67d7d73e0d226c094286babf89cb6988dc23c59f
Cody Piazza <codypiazza@gmail.com>
2025-10-15 14:12:56 -0600

    iso mode

--- module/cdp-workspace-level-components/dbx_external_locations.tf
+++ module/cdp-workspace-level-components/dbx_external_locations.tf
@@ -4,6 +4,7 @@ resource "databricks_external_location" "cdp_catalog_root_ext_loc" {
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_root.name, azurerm_storage_account.cdp_catalog_root_storage_account.name)
   credential_name = local.cdp_storage_credential.id
   owner           = local.workspace_admin_group.display_name
+  isolation_mode  = "ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_catalog_root]
 }
 
@@ -13,6 +14,7 @@ resource "databricks_external_location" "cdp_catalog_bronze_ext_loc" {
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_bronze.name, azurerm_storage_account.cdp_bronze_storage_account.name)
   credential_name = local.cdp_storage_credential.id
   owner           = local.workspace_admin_group.display_name
+  isolation_mode  = "ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_bronze]
 }
 
@@ -22,6 +24,7 @@ resource "databricks_external_location" "cdp_catalog_silver_ext_loc" {
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_silver.name, azurerm_storage_account.cdp_silver_storage_account.name)
   credential_name = local.cdp_storage_credential.id
   owner           = local.workspace_admin_group.display_name
+  isolation_mode  = "ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_silver]
 }
 
@@ -31,6 +34,7 @@ resource "databricks_external_location" "cdp_catalog_gold_ext_loc" {
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_gold.name, azurerm_storage_account.cdp_gold_storage_account.name)
   credential_name = local.cdp_storage_credential.id
   owner           = local.workspace_admin_group.display_name
+  isolation_mode  = "ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_gold]
 }
 
@@ -40,6 +44,7 @@ resource "databricks_external_location" "cdp_staging_inbound_ext_loc" {
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_staging_inbound.name, azurerm_storage_account.cdp_staging_inbound_storage_account.name)
   credential_name = local.cdp_storage_credential.id
   owner           = local.workspace_admin_group.display_name
+  isolation_mode  = "ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_staging_inbound]
 }
 resource "databricks_grant" "staging_inbound_ext_loc" {
@@ -54,6 +59,7 @@ resource "databricks_external_location" "cdp_staging_outbound_ext_loc" {
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_staging_outbound.name, azurerm_storage_account.cdp_staging_outbound_storage_account.name)
   credential_name = local.cdp_storage_credential.id
   owner           = local.workspace_admin_group.display_name
+  isolation_mode  = "ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_staging_outbound]
 }
 resource "databricks_grant" "staging_outbound_ext_loc" {
