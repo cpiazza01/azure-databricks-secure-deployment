bb059d2004beeb4edf3e22e96b1cdf82d7c0d89c
Cody Piazza <codypiazza@gmail.com>
2025-10-16 18:28:22 -0600

    add access for relevant teams

--- module/cdp-workspace-level-components/dbx_access_group_memberships.tf
+++ module/cdp-workspace-level-components/dbx_access_group_memberships.tf
@@ -1,3 +1,15 @@
+# Functions schema
+resource "databricks_group_member" "cdp_functions_group_membership_all_entra_dev" {
+  for_each  = toset([for group in local.cdp_entra_groups : group if env == "dev"])
+  group_id  = local.cdp_functions_group
+  member_id = each.value.id
+}
+resource "databricks_group_member" "cdp_functions_group_membership_all_rw_sps" {
+  for_each  = local.cdp_rw_sps
+  group_id  = local.cdp_functions_group
+  member_id = each.value.id
+}
+
 # Bronze group membership - Platform ingestion team
 resource "databricks_group_member" "cdp_bronze_rw_membership_platform_ingestion_team" {
   count     = var.env == "dev" ? 1 : 0
@@ -83,4 +95,28 @@ resource "databricks_group_member" "cdp_staging_inbound_membership_platform_inge
 resource "databricks_group_member" "cdp_staging_inbound_membership_platform_ingestion_team_rw_sp" {
   group_id  = local.cdp_staging_inbound_group.id
   member_id = local.platform_ingestion_sp_rw.id
+}
+
+# Staging inbound group membership - App teams
+resource "databricks_group_member" "cdp_staging_inbound_membership_app_teams_dev" {
+  for_each  = toset([for group in local.app_team_groups : group if env == "dev"])
+  group_id  = local.cdp_staging_inbound_group.id
+  member_id = each.value.id
+}
+resource "databricks_group_member" "cdp_staging_inbound_membership_app_teams_rw_sp" {
+  for_each  = local.app_team_sps_rw
+  group_id  = local.cdp_staging_inbound_group.id
+  member_id = each.value.id
+}
+
+# Staging outbound group membership - App teams
+resource "databricks_group_member" "cdp_staging_outbound_membership_app_teams_dev" {
+  for_each  = toset([for group in local.app_team_groups : group if env == "dev"])
+  group_id  = local.cdp_staging_outbound_group.id
+  member_id = each.value.id
+}
+resource "databricks_group_member" "cdp_staging_outbound_membership_app_teams_rw_sp" {
+  for_each  = local.app_team_sps_rw
+  group_id  = local.cdp_staging_outbound_group.id
+  member_id = each.value.id
 }
