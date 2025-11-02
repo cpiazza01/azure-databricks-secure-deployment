b40f3ec42736a1cafd2f25b7443250a022e8296c
Cody Piazza <codypiazza@gmail.com>
2025-11-02 09:59:27 -0700

    add addmin stuff

--- module/cdp-account-level-components/dbx_budget_policies.tf
+++ module/cdp-account-level-components/dbx_budget_policies.tf
@@ -1,3 +1,13 @@
+resource "databricks_budget_policy" "budget_policy_ws_admin" {
+  for_each              = databricks_group.entra_groups
+  policy_name           = "1_CDP_WORKSPACE_ADMIN_BUDGET_POLICY_${upper(var.env)}"
+  binding_workspace_ids = [local.workspace_id]
+  custom_tags = [{
+    key   = "Team"
+    value = "CDP_WORKSPACE_ADMIN"
+  }]
+}
+
 resource "databricks_budget_policy" "budget_policies" {
   for_each              = databricks_group.entra_groups
   policy_name           = "${each.value.display_name}_BUDGET_POLICY_${upper(var.env)}"
--- module/cdp-workspace-level-components/dbx_sql_warehouses.tf
+++ module/cdp-workspace-level-components/dbx_sql_warehouses.tf
@@ -1,3 +1,20 @@
+resource "databricks_sql_endpoint" "admin_sql_warehouses" {
+  name                      = "CDP_WORKSPACE_ADMIN Warehouse"
+  cluster_size              = var.env == "prod" ? "X-Small" : "2X-Small"
+  min_num_clusters          = 1
+  max_num_clusters          = 10
+  auto_stop_mins            = 5
+  enable_serverless_compute = true
+  no_wait                   = true
+
+  tags {
+    custom_tags {
+      key   = "Team"
+      value = "CDP_WORKSPACE_ADMIN"
+    }
+  }
+}
+
 resource "databricks_sql_endpoint" "group_sql_warehouses" {
   for_each                  = { for index, group in local.cdp_entra_groups : group.display_name => group }
   name                      = "${each.value.display_name} Warehouse"
