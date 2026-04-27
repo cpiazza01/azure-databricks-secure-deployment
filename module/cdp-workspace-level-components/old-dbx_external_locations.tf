a2fc69b5cc8a88c885f313b53edc4f452c979e9f
Cody Piazza <codypiazza@gmail.com>
2026-04-26 18:43:59 -0600

    debug

--- module/cdp-workspace-level-components/old-dbx_external_locations.tf
+++ module/cdp-workspace-level-components/old-dbx_external_locations.tf
@@ -3,7 +3,7 @@ resource "databricks_external_location" "cdp_catalog_root_ext_loc" {
   name            = "cdp_catalog_root_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_root.name, azurerm_storage_account.cdp_catalog_root_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  owner           = local.cdp_entra_group_workspace_admin.display_name
+  owner           = local.workspace_admin_group.display_name
   isolation_mode  = "ISOLATION_MODE_ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_catalog_root]
 }
@@ -13,7 +13,7 @@ resource "databricks_external_location" "cdp_catalog_bronze_ext_loc" {
   name            = "cdp_catalog_bronze_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_bronze.name, azurerm_storage_account.cdp_bronze_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  owner           = local.cdp_entra_group_workspace_admin.display_name
+  owner           = local.workspace_admin_group.display_name
   isolation_mode  = "ISOLATION_MODE_ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_bronze]
 }
@@ -23,7 +23,7 @@ resource "databricks_external_location" "cdp_catalog_silver_ext_loc" {
   name            = "cdp_catalog_silver_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_silver.name, azurerm_storage_account.cdp_silver_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  owner           = local.cdp_entra_group_workspace_admin.display_name
+  owner           = local.workspace_admin_group.display_name
   isolation_mode  = "ISOLATION_MODE_ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_silver]
 }
@@ -33,7 +33,7 @@ resource "databricks_external_location" "cdp_catalog_gold_ext_loc" {
   name            = "cdp_catalog_gold_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_catalog_storage_gold.name, azurerm_storage_account.cdp_gold_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  owner           = local.cdp_entra_group_workspace_admin.display_name
+  owner           = local.workspace_admin_group.display_name
   isolation_mode  = "ISOLATION_MODE_ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_gold]
 }
@@ -43,9 +43,14 @@ resource "databricks_external_location" "cdp_staging_inbound_ext_loc" {
   name            = "cdp_staging_inbound_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_staging_inbound.name, azurerm_storage_account.cdp_staging_inbound_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  owner           = local.cdp_entra_group_workspace_admin.display_name
+  owner           = local.workspace_admin_group.display_name
   isolation_mode  = "ISOLATION_MODE_ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_staging_inbound]
+}
+resource "databricks_grant" "staging_inbound_ext_loc" {
+  external_location = databricks_external_location.cdp_staging_inbound_ext_loc.id
+  principal         = local.cdp_staging_inbound_group.display_name
+  privileges        = var.cdp_privileges_staging
 }
 
 # Staging Outbound
@@ -53,7 +58,12 @@ resource "databricks_external_location" "cdp_staging_outbound_ext_loc" {
   name            = "cdp_staging_outbound_ext_loc_${lower(var.env)}"
   url             = format("abfss://%s@%s.dfs.core.windows.net", azurerm_storage_container.cdp_staging_outbound.name, azurerm_storage_account.cdp_staging_outbound_storage_account.name)
   credential_name = local.cdp_storage_credential.id
-  owner           = local.cdp_entra_group_workspace_admin.display_name
+  owner           = local.workspace_admin_group.display_name
   isolation_mode  = "ISOLATION_MODE_ISOLATED"
   depends_on      = [azurerm_role_assignment.storage_account_access_staging_outbound]
+}
+resource "databricks_grant" "staging_outbound_ext_loc" {
+  external_location = databricks_external_location.cdp_staging_outbound_ext_loc.id
+  principal         = local.cdp_staging_outbound_group.display_name
+  privileges        = var.cdp_privileges_staging
 }
